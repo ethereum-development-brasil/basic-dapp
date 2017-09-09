28f16b3e087a8c3ee7474b412a376c0f4e54de3c
André Ferreira <contato@andreferreira.me>
2017-09-09 17:28:26 -0300

    interacting with contract

---
+++ contract.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.13;
+
+contract MyContract {
+    address creator;
+    uint256 myNumber;
+
+    function MyContract() {
+        creator = msg.sender;
+        myNumber = 3;
+    }
+
+    function getCreator() constant returns(address) {
+        return creator;
+    }
+
+    function getNumber() constant returns(uint256) {
+        return myNumber;
+    }
+
+    function setMyNumber(uint256 myNewNumber) {
+        myNumber = myNewNumber;
+    }
+
+    function kill() {
+        if (msg.sender == creator) {
+            suicide(creator);
+        }
+    }
+}
